8b2818020ae527c335c4ec58faeef6311585aecb
Gray Calhoun <gray@clhn.co>
2013-11-29 01:01:46 -0600

    Switch to literate-style coding for bootstrap
    This commit switches the code to be embedded in the LaTeX source
    and then extracts it with knitr.

--- asymptotics/bootstrap_fig2.R
+++
@@ -1,9 +0,0 @@
-set.seed(76983)
-source("asymptotics/bootstrap_setup.R")
-
-pdf(file = "asymptotics/bootstrap_fig2.pdf", width=8, height=3)
-par(mfrow = c(1,3))
-
-nicehist(rexp, 2)
-nicehist(rexp, 15)
-nicehist(rexp, 50)
